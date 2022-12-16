459e26e515dc9413a4b7c6a8bb9ad54b9809fabe
Camila Neira <neiracamila10@gmail.com>
2022-12-16 11:18:20 -0300

    06 db definition

---
+++ db-definition.sql
@@ -0,0 +1,16 @@
+ create database if not exists empleados;
+ use empleados;
+ 
+ create table empleados (
+     id int not null auto_increment,
+     nombre varchar(255), 
+     correo varchar(255), 
+     foto varchar (5000), 
+     primary key (id)
+     );
+
+
+-- insert  into empleados (nombre, correo, foto) values
+-- ('Mario', 'mario@email.com', 'fotodemario.jpg');
+     
+-- select * from empleados;
